c7c37cea03ae514a243e65084d227fc3ddcb599f
Rina Hayasaka <themunyang21@naver.com>
2025-07-02 07:27:49 +0900

    소소한 수정

--- check.js
+++ check.js
@@ -21,13 +21,13 @@ function doCheckHost(){
         var hostInfoHead = document.getElementById("host-head");
         hostInfoHead.textContent = `${data.hostname}에 대한 결과`;
 
-        var ip4Addr = (typeof data.resolv_addr.v4 != "undefined") ? data.resolv_addr.v4 : "감지되지 않음";
+        var ip4Addr = (typeof data.resolv_addr.v4 != "undefined" || data.resolv_addr.v4 != null) ? data.resolv_addr.v4 : "감지되지 않음";
         var ip4Text = document.createTextNode(`감지된 IPv4 주소: ${ip4Addr}`);
         var ip4Display = document.createElement("li");
         ip4Display.appendChild(ip4Text);
         resList.appendChild(ip4Display);
         if(check_ip6 !== false){
-         var ip6Addr = (typeof data.resolv_addr.v6 != "undefined") ? data.resolv_addr.v6 : "감지되지 않음";
+         var ip6Addr = (typeof data.resolv_addr.v6 != "undefined" || data.resolv_addr.v6 != null) ? data.resolv_addr.v6 : "감지되지 않음";
          var ip6Text = document.createTextNode(`감지된 IPv6 주소: ${ip6Addr}`);
          var ip6Display = document.createElement("li");
          ip6Display.appendChild(ip6Text);
@@ -35,14 +35,14 @@ function doCheckHost(){
         }
 
         var isUsingCF = data.is_cloudflare;
-        var isUsingCFText = document.createTextNode(isUsingCF ? "Cloudflare 사용 여부: 사용 중" : "Cloudflare 사용 여부: 감지되지 않음");
+        var isUsingCFText = document.createTextNode(isUsingCF ? "Cloudflare 사용 여부: 사용 중" : "Cloudflare 사용 여부: 사용하지 않음");
         var isUsingCFDisplay = document.createElement("li");
         isUsingCFDisplay.appendChild(isUsingCFText);
         resList.appendChild(isUsingCFDisplay);
 
         if(isUsingCF !== false && check_cdn !== false){
             var edgeRegion = data.cf_hostinfo.edge_region;
-            var edgeRegionText = document.createTextNode(`감지된 엣지 로케이션 코드: ${edgeRegion}`);
+            var edgeRegionText = document.createTextNode(`엣지 로케이션 코드: ${edgeRegion}`);
             var edgeRegionDisplay = document.createElement("li");
             edgeRegionDisplay.appendChild(edgeRegionText);
             resList.appendChild(edgeRegionDisplay);
--- index.php
+++ index.php
@@ -8,9 +8,14 @@
 <meta name="color-scheme" content="light dark" />
 <meta name="fediverse:creator" content="@Rina@uri.life" />
 <script src="./check.js"></script>
+<link rel="stylesheet" href="./main.css" />
 </head>
 <body id="mainpagebody">
 
+<h2>단순 Cloudflare 사용 검사기</h2>
+
+<p>이 페이지는 웹사이트가 Cloudflare를 사용하는지 여부를 검사하는 간단한 도구입니다.
+<br />아래 입력란에 도메인명을 입력하고 "Scan" 버튼을 클릭하면 해당 도메인이 Cloudflare를 사용하고 있는지 확인할 수 있습니다.</p>
 
 <div id="check-form">
 도메인명 : <input type="text" name="hostname" id="host" placeholder="www.example.com" />
@@ -22,8 +27,7 @@
 
 <h2><span id="host-head"></span></h2>
 <div id="result">
-<ul id="result-list">
-</ul>
+ <ul id="result-list"></ul>
 </div>
 
 </body>
